ba17f3796ddd35ad9ef958b54ac0e3d0464f0868
hassy <andruhinadaria@yandex.ru>
2022-08-08 14:18:10 +0300

    game.no win

--- game.js
+++ game.js
@@ -6,5 +6,35 @@ $('#start .btn_start').click(function () {
     $('#info #now_go .count').text($player);
 })
 
+$('#game #game_body .point').click(function () {
+    //нельзя нажать на уже занятую ячейку
+    if (!$(this).hasClass('active')){
+        $(this).addClass('active');
+        $player_count = 'p_'+$player;
+        $(this).addClass($player_count);
+        //получение координат
+        $x = $(this).data('x');
+        $y = $(this).data('y');
 
+        //Добавление хода
+        $player_counts = Number($('#info #' + $player_count + ' .go_count .count').text()) + 1;
+        $('#info #' + $player_count + ' .go_count .count').text($player_counts);
+
+        //переход хода
+        if ($player==1){
+            $player = 2;
+            $('#info #now_go .count').text($player);
+        }
+        else{
+            $player = 1;
+            $('#info #now_go .count').text($player);
+        }
+
+        //Проверка на победу
+
+    }
+    else{
+        console.log('has active class')
+    }
+})
 
