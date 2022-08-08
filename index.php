7599e9a579e5c68bf185f6162453b9f78fc93dc5
hassy <andruhinadaria@yandex.ru>
2022-08-08 10:38:41 +0300

    adder tags for info player

--- index.php
+++ index.php
@@ -7,7 +7,22 @@
     <script src="jquery-3.3.1.min.js"></script>
 </head>
 <body>
+<div id="game">
+    <div id="info">
+        <p id="now_go">Сейчас ходит игрок № <span class="count"></span></p>
+        <div id="p_1" class="player_info">
+            <p class="title">Игрок 1</p>
+            <p class="go_count">Ходов: <span class="count"></span></p>
+        </div>
+        <div id="p_2" class="player_info">
+            <p class="title">Игрок 2</p>
+            <p class="go_count">Ходов: <span class="count"></span></p>
+        </div>
+    </div>
+    <div id="game_body">
 
+    </div>
+</div>
 <script src="game.js"></script>
 </body>
 </html>
