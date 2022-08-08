614a3add820307d792c7a9a1ff2012f3edb43e93
hassy <andruhinadaria@yandex.ru>
2022-08-08 10:29:06 +0300

    adder start tags

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Game</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="jquery-3.3.1.min.js"></script>
+</head>
+<body>
+
+<script src="game.js"></script>
+</body>
+</html>
